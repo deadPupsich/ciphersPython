40cbb935f4bea916a481d992b63e97c6786b0a57
Konstantin Belousov <kibelousov@mail.ru>
2022-12-22 17:31:38 +0300

    mod

--- RSA cipher/fermamethod.py
+++ RSA cipher/fermamethod.py
@@ -1,9 +1,31 @@
 import math
+"""
+!!!Важно
+Метод Ферма для криптоанализа RSA применим в том случае, если
+параметры р и q оказались близки друг к другу
+"""
+def keys(p: int, q: int, a: int):
+    '''
+    Def keys генерирует ключи.
+    '''
+    mod = p * q
+    fi = (p - 1) * (q - 1)
+    while True:
+        if math.gcd(a, fi) == 1:
+            break
+        else:
+            print('Попробуйте другое число.')
+    d = 0
+    for i in range(1, mod):
+        if (a * i) % fi == 1:
+            d = i
+            break
+    return d
 
-a, b = int(input()), int(input())
+a, b = input('Введите открытый ключ: ').split()
+a, b = int(a), int(b)
 b1 = b
 b = round(math.sqrt(b)) + 1
-print(b)
 c = 0
 cnt = 1
 while True:
@@ -18,4 +40,4 @@ while True:
 
 p = b + c
 q = b - c 
-print(f'p = {p} q = {q} N = {int(p * q)}')
+print(f' p = {p}\n q = {q}\n N = {int(p * q)}\n Закрытый Ключ - {keys(int(p), int(q), a)}')
